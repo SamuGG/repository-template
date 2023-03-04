7766571081ef948f31680bc6d502c44c38727a1e
SamuGG <SamuGG@users.noreply.github.com>
2023-03-04 19:37:24 +0000

    chore: Set Yarn telemetry off

--- template-repo-scripts/postCreateCommand.sh
+++ template-repo-scripts/postCreateCommand.sh
@@ -2,4 +2,5 @@
 sudo chown vscode .yarn && \
 bash template-repo-scripts/generate-global-gitignore.sh && \
 bash template-repo-scripts/configure-global-gitconfig.sh && \
-make clean install
+make clean install && \
+yarn config set --home enableTelemetry 0
