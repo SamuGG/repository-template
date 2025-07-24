a957fe5d4db54768cbc9b3d24f8f4ead39b02213
SamuGG <SamuGG@users.noreply.github.com>
2025-07-25 00:25:48 +0100

    chore: Remove repository configuration scripts

--- template-repo-scripts/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-sudo chown vscode .yarn && \
-bash template-repo-scripts/generate-global-gitignore.sh && \
-bash template-repo-scripts/configure-global-gitconfig.sh && \
-make clean install && \
-yarn config set --home enableTelemetry 0
